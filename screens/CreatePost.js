f9ae016dffb46308f3bd5425411aef65fb6f8da6
RishonAnthony <70851332+Rishonanthony22@users.noreply.github.com>
2023-03-13 21:22:55 +0530

    the changes are made

--- screens/CreatePost.js
+++ screens/CreatePost.js
@@ -46,11 +46,11 @@ export default class CreateStory extends Component {
     if (this.state.fontsLoaded) {
       SplashScreen.hideAsync();
       let preview_images = {
-        image_1: require("../assets/story_image_1.png"),
-        image_2: require("../assets/story_image_2.png"),
-        image_3: require("../assets/story_image_3.png"),
-        image_4: require("../assets/story_image_4.png"),
-        image_5: require("../assets/story_image_5.png"),
+        image_1: require("../assets/image_1.png"),
+        image_2: require("../assets/image_2.png"),
+        image_3: require("../assets/image_3.png"),
+        image_4: require("../assets/image_4.png"),
+        image_5: require("../assets/image_5.png"),
       };
       return (
         <View style={styles.container}>
